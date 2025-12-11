2ff7dddd6904a36d9420a39af9cb2d2eb47b318d
PRBDUDE <programmerroadblocks@gmail.com>
2025-12-11 10:12:40 -0500

    Clean up comparator

--- src/prb/org/timecard/SortByPunchIn.java
+++ src/prb/org/timecard/SortByPunchIn.java
@@ -2,11 +2,8 @@ package prb.org.timecard;
 
 import java.util.Comparator;
 
-public class SortByPunchIn implements Comparator {
-    public int compare(Object obj1, Object obj2) {
-        TimeCard card1 = (TimeCard) obj1;
-        TimeCard card2 = (TimeCard) obj2;
-
+public class SortByPunchIn implements Comparator<TimeCard> {
+    public int compare(TimeCard card1, TimeCard card2) {
         if (card1.getPunchIn().isBefore(card2.getPunchIn())) return -1;
         if (card1.getPunchIn().isAfter(card2.getPunchIn())) return 1;
         return 0;
